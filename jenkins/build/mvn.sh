9c6e831a0061162eec2e113f0e4aafb20a69b803
Renan Muraga <85200216+renanmuraga@users.noreply.github.com>
2021-11-11 23:52:57 -0300

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,4 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 WORKSPACE=/home/ec2-user/jenkins-data/jenkins_home/workspace/vorx-backend-app-pipeline
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
